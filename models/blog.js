bc71b11f4b610f5c9b649cbb1c0c9390e449f207
pihu26112005 <161943145+pihu26112005@users.noreply.github.com>
2024-04-16 01:27:03 +0530

    Update blog.js

--- models/blog.js
+++ models/blog.js
@@ -1,29 +1,37 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const blogschema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  body : {
-    type : String,
-    required : true
-  },
-  backcoverURL : {
-    type : String,
-    default : '/images/default.png'
-  },
-    createdby : {
-        type : mongoose.Schema.Types.ObjectId,
-        required : true,
-        ref: 'user'
-    },
-},
-{ 
-    timestamps : true
-});
-const BLOG = mongoose.model('blog', blogschema);
+// const blogschema = new mongoose.Schema({
+//   title: {
+//     type: String,
+//     required: true
+//   },
+//   body : {
+//     type : String,
+//     required : true
+//   },
+//   backcoverURL : {
+//     type : String,
+//     default : '/images/default.png'
+//   },
+//     createdby : {
+//         type : mongoose.Schema.Types.ObjectId,
+//         required : true,
+//         ref: 'user'
+//     },
+// },
+// { 
+//     timestamps : true
+// });
+// const BLOG = mongoose.model('blog', blogschema);
 
+// module.exports = BLOG; 
+
+const { MongoClient } = require('mongodb');
+const url = process.env.URL || 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+const dbName = 'blog-globe';
+client.connect();
+const db = client.db(dbName);
+
+BLOG = db.collection('blogs');
 module.exports = BLOG; 
-
- 
