a061e13bda6c27eb5643b39695dace952ef2d7f9
pihu26112005 <161943145+pihu26112005@users.noreply.github.com>
2024-04-16 01:27:43 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,61 +1,101 @@
-const {createHash , randomBytes} = require('crypto');
+// const {createHash , randomBytes} = require('crypto');
+// const {creatTokenforUser,validateToken} = require('../services/auth');
+
+// const mongoose = require('mongoose');
+
+// const userSchema = new mongoose.Schema({
+//     firstname : {
+//         type : String,
+//         required : true
+//     },
+//     email : {
+//         type : String,
+//         required : true,
+//         unique : true
+//     },
+//     // to hash password
+//     secret : {
+//         type : String,
+//         // required : true
+//     },
+//     password : {
+//         type : String,
+//         required : true
+//     },
+//     profileImageURL : {
+//         type : String,
+//         default : '/images/default.png'
+//     },
+//     role : {
+//         type : String,
+//         enum : ['admin','user'], // matlab hmara role sirf in do me se hi value le skta hai 
+//         default : 'user'
+//     },
+// },
+// { 
+//     timestamps : true
+// });
+
+// // userschema.pre tab chlega jab hmare model me ek new schema bnke save hoga 
+// userSchema.pre('save',function(next){
+//     if(this.isModified('password')){ // yha this --> user hai , jo just abhi create hua hai jiske liye ye function v call hua hai
+//         this.secret = randomBytes(20).toString('hex');
+//         this.password = createHash('sha256',this.secret).update(this.password).digest('hex');
+//         // createHash me hme hashing algo,secret key deni pdti hai , fir btana hota hai ki kya update krna hai , fir digest krna hota hai
+//     }
+//     next();
+// });
+
+// // ye schema pr ek function bnaya hai , isme phle uska naam aur baadme uski body pass krte hai 
+// // ye function hum khi aur kisi schema pe call kr skte hai 
+// userSchema.static("matchpasswordandGenerateToken", async function (email,password){
+//     const user = await this.findOne ({email});
+//     if(!user) throw new Error('user not found')
+
+//     const secret = user.secret;
+//     const oldhashedpassword = user.password;
+
+//     const newhashedpassword = createHash('sha256',secret).update(password).digest('hex');
+
+//     if (oldhashedpassword !== newhashedpassword)
+//         throw new Error("incorrect password")
+
+//     const token = creatTokenforUser(user);
+
+//     return token;
+// });
+
+// const USER = mongoose.model('user', userSchema);
+
+// module.exports = USER; 
+
 const {creatTokenforUser,validateToken} = require('../services/auth');
+const { MongoClient } = require('mongodb');
+const { randomBytes, createHash } = require('crypto');
+const url = process.env.URL || 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+const dbName = 'blog-globe';
+client.connect();
+const db = client.db(dbName);
 
-const mongoose = require('mongoose');
+const USER = db.collection('users');
 
-const userSchema = new mongoose.Schema({
-    firstname : {
-        type : String,
-        required : true
-    },
-    email : {
-        type : String,
-        required : true,
-        unique : true
-    },
-    // to hash password
-    secret : {
-        type : String,
-        // required : true
-    },
-    password : {
-        type : String,
-        required : true
-    },
-    profileImageURL : {
-        type : String,
-        default : '/images/default.png'
-    },
-    role : {
-        type : String,
-        enum : ['admin','user'], // matlab hmara role sirf in do me se hi value le skta hai 
-        default : 'user'
-    },
-},
-{ 
-    timestamps : true
-});
-
-// userschema.pre tab chlega jab hmare model me ek new schema bnke save hoga 
-userSchema.pre('save',function(next){
-    if(this.isModified('password')){ // yha this --> user hai , jo just abhi create hua hai jiske liye ye function v call hua hai
-        this.secret = randomBytes(20).toString('hex');
-        this.password = createHash('sha256',this.secret).update(this.password).digest('hex');
-        // createHash me hme hashing algo,secret key deni pdti hai , fir btana hota hai ki kya update krna hai , fir digest krna hota hai
+async function saveUser(user) {
+    if(user.password){
+        user.secret = randomBytes(20).toString('hex');
+        user.password = createHash('sha256').update(user.password + user.secret).digest('hex');
     }
-    next();
-});
+    return await usersCollection.insertOne(user);
+}
 
-// ye schema pr ek function bnaya hai , isme phle uska naam aur baadme uski body pass krte hai 
-// ye function hum khi aur kisi schema pe call kr skte hai 
-userSchema.static("matchpasswordandGenerateToken", async function (email,password){
-    const user = await this.findOne ({email});
+async function matchpasswordandGenerateToken(email, password) {
+    const user = await usersCollection.findOne({email});
     if(!user) throw new Error('user not found')
 
     const secret = user.secret;
     const oldhashedpassword = user.password;
 
-    const newhashedpassword = createHash('sha256',secret).update(password).digest('hex');
+    const newhashedpassword = createHash('sha256').update(password + secret).digest('hex');
 
     if (oldhashedpassword !== newhashedpassword)
         throw new Error("incorrect password")
@@ -63,10 +103,6 @@ userSchema.static("matchpasswordandGenerateToken", async function (email,passwor
     const token = creatTokenforUser(user);
 
     return token;
-});
+}
 
-const USER = mongoose.model('user', userSchema);
-
-module.exports = USER; 
-
- 
+module.exports = { USER, saveUser, matchpasswordandGenerateToken };
