f1bd72c9f3fcc06301f8c7a29f962e5b72098bec
pihu26112005 <161943145+pihu26112005@users.noreply.github.com>
2024-04-16 01:27:24 +0530

    Update comment.js

--- models/comment.js
+++ models/comment.js
@@ -1,28 +1,36 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const commentschema = new mongoose.Schema({
-  content: {
-    type: String,
-    required: true
-  },
-  blogid: {
-        type : mongoose.Schema.Types.ObjectId,
-        required : true,
-        ref: 'blog'
-  },
-    createdby : {
-        type : mongoose.Schema.Types.ObjectId,
-        required : true,
-        ref: 'user'
-    },
-},
-{ 
-    timestamps : true
-});
+// const commentschema = new mongoose.Schema({
+//   content: {
+//     type: String,
+//     required: true
+//   },
+//   blogid: {
+//         type : mongoose.Schema.Types.ObjectId,
+//         required : true,
+//         ref: 'blog'
+//   },
+//     createdby : {
+//         type : mongoose.Schema.Types.ObjectId,
+//         required : true,
+//         ref: 'user'
+//     },
+// },
+// { 
+//     timestamps : true
+// });
 
 
-const COMMENT = mongoose.model('comment', commentschema);
+// const COMMENT = mongoose.model('comment', commentschema);
 
+// module.exports = COMMENT; 
+
+const { MongoClient } = require('mongodb');
+const url = process.env.URL || 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+const dbName = 'blog-globe';
+client.connect();
+const db = client.db(dbName);
+
+COMMENT = db.collection('comments');
 module.exports = COMMENT; 
-
- 
